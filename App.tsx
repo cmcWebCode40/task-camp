54a713756af593f12cf076d7053a5e24710a4dfa
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2023-06-14 00:00:32 +0100

    chore:added required packages

--- App.tsx
+++ App.tsx
@@ -1,15 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import { MainNavigation } from './navigations';
 import React from 'react';
-import { StyledText, StyledView } from './components/StyledRN';
+import { StyledView } from './components/StyledRN';
 
 const App = (): React.ReactElement => {
   return (
-    <StyledView className='flex-1 items-center justify-center bg-slate-600 '>
-      <StyledText className='text-red-500 font-bold text-2xl'>
-        Open up App.tsx to start working on your app!
-      </StyledText>
-      <StyledText>Open up App.tsx to start working on your app!</StyledText>
+    <StyledView className='flex-1'>
       <StatusBar style='auto' />
+      <MainNavigation />
     </StyledView>
   );
 };
