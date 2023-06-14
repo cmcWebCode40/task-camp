aae480935a60d1a39cc1d791f71b0bf80709acbb
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2023-06-14 22:16:43 +0100

    chore: modified implemented atomic components logic

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -1,6 +1,7 @@
-import { StyledText, StyledPressable } from 'components/StyledRN';
 import React from 'react';
 import { PressableProps } from 'react-native';
+
+import { StyledPressable, StyledText, StyledView } from 'components/StyledRN';
 
 type Size = 'sm' | 'md' | 'lg';
 type ButtonVariant = 'contained' | 'outlined' | 'text';
@@ -9,6 +10,7 @@ interface ButtonProps extends PressableProps {
   variant?: ButtonVariant;
   size?: Size;
   className?: string;
+  icon?: React.ReactNode;
   children: React.ReactNode;
 }
 
@@ -19,7 +21,7 @@ const variantStyles = {
 };
 
 const variantSizes = {
-  md: 'h-10 py-2 px-4',
+  md: 'h-12 py-2 px-4',
   sm: 'h-9 px-2',
   lg: 'h-11 px-8',
 };
@@ -31,22 +33,23 @@ const buttonTextClass = {
 };
 
 const Button: React.FunctionComponent<ButtonProps> = ({
-  style,
   size = 'md',
   variant = 'contained',
   className,
   children,
+  icon,
   ...rest
 }) => {
   return (
     <StyledPressable
-      className={`flex items-center justify-center rounded hover:bg-slate-300
+      className={`flex-row items-center justify-center bg-white shadow-md rounded space-x-2 hover:bg-slate-300
       active:bg-slate-500
- ${variantSizes[size]} ${variantStyles[variant]}`}
+ ${variantSizes[size]} ${variantStyles[variant]} ${className}`}
       {...rest}
     >
+      {icon && <StyledView>{icon}</StyledView>}
       <StyledText
-        className={`${buttonTextClass[variant]} font-semibold text-center`}
+        className={`${buttonTextClass[variant]} text-base font-semibold text-center flex-row items-center justify-center`}
       >
         {children}
       </StyledText>
--- components/atoms/Icon.tsx
+++ components/atoms/Icon.tsx
@@ -1,8 +1,10 @@
 import {
-  Ionicons,
   AntDesign,
+  Feather,
   FontAwesome,
   FontAwesome5,
+  Ionicons,
+  MaterialIcons,
 } from '@expo/vector-icons';
 import React from 'react';
 import { SvgProps } from 'react-native-svg';
@@ -17,7 +19,12 @@ export type IconName =
   | 'settings-outline'
   | 'close'
   | 'history-outline'
+  | 'ios-time-outline'
   | 'close'
+  | 'search1'
+  | 'add-task'
+  | 'date-range'
+  | 'edit'
   | 'history';
 
 type IconProps = {
@@ -30,32 +37,43 @@ const Icon: React.FunctionComponent<IconProps> = ({
   size = 24,
   ...otherProps
 }) => {
-  switch (name) {
-    case 'settings':
-      return <Ionicons name={name} size={size} {...otherProps} />;
-    case 'settings-outline':
-      return <Ionicons name={name} size={size} {...otherProps} />;
-    case 'star':
-      return <AntDesign name={name} size={size} {...otherProps} />;
-    case 'star':
-      return <AntDesign name={name} size={size} {...otherProps} />;
-    case 'staro':
-      return <AntDesign name={name} size={size} {...otherProps} />;
-    case 'ios-home-outline':
-      return <Ionicons name={name} size={size} {...otherProps} />;
-    case 'chevron-back':
-      return <Ionicons name={name} size={size} {...otherProps} />;
-    case 'history-outline':
-      return <FontAwesome name={'history'} size={size} {...otherProps} />;
-    case 'history':
-      return <FontAwesome5 name={'history'} size={size} {...otherProps} />;
-    case 'close':
-      return <Ionicons name={name} size={size} {...otherProps} />;
-    case 'home':
-      return <Ionicons name={name} size={size} {...otherProps} />;
-    default:
-      throw new Error(`Icon wit name ${name} is  not supported`);
+  const AppIcon = {
+    settings: <Ionicons name='settings' size={size} {...otherProps} />,
+    'settings-outline': (
+      <Ionicons name='settings-outline' size={size} {...otherProps} />
+    ),
+    search1: <AntDesign name='search1' size={size} {...otherProps} />,
+    star: <AntDesign name='star' size={size} {...otherProps} />,
+    staro: <AntDesign name='staro' size={size} {...otherProps} />,
+    'ios-home-outline': (
+      <Ionicons name='ios-home-outline' size={size} {...otherProps} />
+    ),
+    'chevron-back': (
+      <Ionicons name='chevron-back' size={size} {...otherProps} />
+    ),
+    'ios-time-outline': (
+      <Ionicons name='ios-time-outline' size={size} {...otherProps} />
+    ),
+    'history-outline': (
+      <FontAwesome name='history' size={size} {...otherProps} />
+    ),
+    'add-task': <MaterialIcons name='add-task' size={size} {...otherProps} />,
+    'date-range': (
+      <MaterialIcons name='date-range' size={size} {...otherProps} />
+    ),
+    history: <FontAwesome5 name='history' size={size} {...otherProps} />,
+    edit: <Feather name='edit' size={size} {...otherProps} />,
+    close: <Ionicons name='close' size={size} {...otherProps} />,
+    home: <Ionicons name='home' size={size} {...otherProps} />,
+  };
+
+  const icon = AppIcon[name];
+
+  if (!icon) {
+    throw new Error(`Icon wit name ${name} is  not supported`);
   }
+
+  return AppIcon[name];
 };
 
 export default Icon;
